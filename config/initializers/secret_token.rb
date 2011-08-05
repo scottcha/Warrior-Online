ce7599f553d5f18eed57aa7674f139c45d513bff
Scott Chamberlin <scott@hibabysoftware.com>
2011-08-05 05:57:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarriorOnline::Application.config.secret_token = '0828a454550557241c3c288ad92eedfb33009ccd5baf890013939365efc9ba8a378042050260731980cd93bc58f29feb1261834df23a709367e66202c7244035'

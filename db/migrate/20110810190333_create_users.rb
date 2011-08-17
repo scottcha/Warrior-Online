5946c29d79ad61e52912fbd189bbfc395f0185ed
Scott Chamberlin <scott@hibabysoftware.com>
2011-08-16 21:09:38 -0700

    updated migrate to create users table

--- db/migrate/20110810190333_create_users.rb
+++ db/migrate/20110810190333_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
-    alter_table :users do |t|
+    create_table :users do |t|
       t.string :firstName
       t.string :lastName
       t.string :nickName
